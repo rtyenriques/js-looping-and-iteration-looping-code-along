0ad79d4f4f73013c36a5143eaf0e89baf85f5ec1
= <rtyenriques@gmail.com>
2021-11-23 15:04:06 -0800

    commit tree 324774db7b7f759a37828fc5e3ff081274ebdcf8

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+// writeCards(["Ada", "Brendan", "Ali"], "birthday");
+// [
+//     "Thank you, Ada, for the wonderful birthday gift!",
+//     "Thank you, Brendan, for the wonderful birthday gift!",
+//     "Thank you, Ali, for the wonderful birthday gift!"
+//   ];
+
+  function writeCards(people, event) {
+      const newArray = []
+      for (let i = 0; i < people.length; i++) {
+        newArray.push(`Thank you, ${people[i]}, for the wonderful ${event} gift!`);
+      }
+    return newArray
+  }
+
+  function countDown(num) {
+    let i = 0
+    while (i < 11) {
+      console.log(i);
+      i++
+    }
+    return num
+
+  }
